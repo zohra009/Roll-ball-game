01e1e2d571b69d9a9a93426de2642312b06be858
zor009 <anzohra@outlook.com>
2019-07-17 17:20:06 -0400

    added to script
    added tag coded to script

--- Roll Ball Game/Assets/Script/Player_Controller.cs
+++ Roll Ball Game/Assets/Script/Player_Controller.cs
@@ -23,4 +23,12 @@ public class Player_Controller : MonoBehaviour{
     Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
     rb.AddForce (movement * speed);
   }
+  void OnTriggerEnter(Collider other) 
+  {
+    if(other.gameObject.CompareTag("Pick Up"))
+    {
+      other.gameObject.SetActive(false);
+    }
+  }
 }
+  
