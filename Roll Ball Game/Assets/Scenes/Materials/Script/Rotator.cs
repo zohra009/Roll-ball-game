7d30e47a52ccecd9b3118a0fa59c5487af22db61
zor009 <anzohra@outlook.com>
2019-07-17 14:16:39 -0400

    pick ups
    added rotation to pick up object

--- Roll Ball Game/Assets/Scenes/Materials/Script/Rotator.cs
+++ Roll Ball Game/Assets/Scenes/Materials/Script/Rotator.cs
@@ -8,6 +8,6 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime); 
     }
 }
